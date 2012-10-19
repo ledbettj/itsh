88159886a9411ef891dd2ddcebf4454ca14e0bd1
John Ledbetter <john.ledbetter@gmail.com>
2012-10-19 16:39:00 -0400

    add missing headers

--- shell.c
+++ shell.c
@@ -1,11 +1,14 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <pwd.h>
 #include <errno.h>
 #include <readline/readline.h>
+#include <readline/history.h>
 #include "shell.h"
 #include "parser.h"
 #include "builtin.h"
